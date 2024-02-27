28cc4dcbc69ec1703fbcef96a7b4fd69d330ec56
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-02-27 11:55:58 +0000

    Update test.py

--- test.py
+++ test.py
@@ -8,14 +8,6 @@ def fail():
     json.dumps(a)
 
 def goto(linenum):
-    """
-    This function sets the value of the `line` global variable to the input `linenum`.
-
-    Args:
-        linenum (int): The `linenum` input parameter sets the line number of the
-            code to which the function will jump.
-
-    """
     global line
     line = linenum
 
