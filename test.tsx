c486edd0cae9d7ec11a44d3640a75f568e7f7470
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-04-12 11:15:26 +0100

    Update test.tsx

--- test.tsx
+++ test.tsx
@@ -4,17 +4,6 @@ interface HelloWorldProps {
   name: string;
 }
 
-/**
- * @description This function creates a React component that displays a greeting and
- * a paragraph of text when the component is rendered.
- * 
- * @param { string } .name - The `.name` input parameter represents the name of the
- * person being greeted in the function. It is used to personalize the greeting message.
- * 
- * @returns { Component } The function returns a JSX element with an `<h1>` tag
- * displaying the name of the user, and a `<p>` tag stating "Welcome to my TypeScript
- * JSX example."
- */
 const HelloWorld: React.FC<HelloWorldProps> = ({ name }) => {
   return (
     <div>
